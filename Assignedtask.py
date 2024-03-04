20d5e4027147534849156d102c76a3fce4d0655a
PEM TSHERI <02230102.cst@rub.edu.bt>
2024-03-04 23:42:55 +0530

    updated

--- Assignedtask.py
+++ Assignedtask.py
@@ -4,11 +4,8 @@ print("----------------------------------------------------")
 age = int(input("Enter your age:"))
 std = input("Are you a student? (yes/no):")
 
-if age <= 12:
+if age <= 12 or 13 <= age <= 18 and std == "yes":
     print("Congrats! You are eligible for a discount on movie ticket. ")
-
-elif 13 <= age <= 18 and std == "yes":
-    print("Congrats! You are eligible for a discount on movie ticket.")
-
+    
 else:
     print("Sorry! You are not eligible for a discount on movie ticket.")
