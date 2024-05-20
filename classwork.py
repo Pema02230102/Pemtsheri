b3a890a5a5a7a6c8adc7948f5d581b0cb58224f6
PEM TSHERI <02230102.cst@rub.edu.bt>
2024-05-20 14:40:13 +0600

    classwork

---
+++ classwork.py
@@ -0,0 +1,17 @@
+class dog:
+    def __init__(self, name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
+
+print("Information about my pets") 
+dawa = dog("dawa", "changlu", 12)
+nima = dog("nima", "serbu", 13)
+blacky = dog("blacky", "yenglu", 15)
+
+print(dawa.name, dawa.color, dawa.age)
+print(nima.name, nima.color, nima.age)
+print(blacky.name, blacky.color, blacky.age)
+
+
+
